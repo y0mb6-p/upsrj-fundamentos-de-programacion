4f6865cf30af3c2e3fd61478a3a358331a6e0974
y0mb6-p <ay0.mb6@gmail.com>
2026-01-25 23:57:48 -0600

    Determinar Par o Impar

--- src/main.c
+++ src/main.c
@@ -1,13 +1,43 @@
 #include <stdio.h>
+#include <math.h>
+
+int is_even(int num)
+{
+    if (num % 2==0)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
 
 int main()
 {
-    float s = 0.0;
-    printf("Enter the length of the side: ");
-    scanf("%f",&s);
-    float area=s*s;
-    float vol=s*s*s;
-    printf("The area of the square is = %.2f",area);
-    printf("\nThe volume of the cube is = %.2f\n",vol);
-    return 0;
+    int num = 0;
+    printf("Enter an integer number: ");
+    scanf("%i",&num);
+
+    int par = is_even(num);
+    if (par==1)
+    {
+        printf("El numero es par");
+    }
+    else
+    {
+        printf("El numero es impar");
+    }
 }
+
+//int areayvolumen()
+//{
+    //float s = 0.0;
+    //printf("Enter the length of the side: ");
+    //scanf("%f",&s);
+    //float area=s*s;
+    //float vol=s*s*s;
+    //printf("The area of the square is = %.2f",area);
+    //printf("\nThe volume of the cube is = %.2f\n",vol);
+    //eturn 0;
+//}
